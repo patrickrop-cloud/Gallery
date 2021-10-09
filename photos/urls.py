8123d4130f2aa84cff8e9e621a7b8ad17da4f0b0
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-09 23:47:35 +0300

    configure urls and views

---
+++ photos/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+   url('^$',views.gallery,name='gallery'),
+    path('photo/<str:pk>',views.viewPhoto,name='photo'),
+    url('add',views.addPhoto,name='add'),
+    url('about',views.about,name='about'),
+    url(r'^search/', views.search_results, name='search_results')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
