83f2cd99e1391efad32aa3fd648d1a2daa99f580
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-11 09:18:23 +0300

    add test to save, delete and update the image

---
+++ README.md
@@ -0,0 +1,31 @@
+##Application name :
+Gallery App
+
+#Developed and Authored By:
+Patrick Rop on October, 2021.
+
+#Description
+This is a python/django project to allow users to create their own categories after signing into the app, upload and view the photos they have uploaded and their descriptions.
+
+#Setup/Installation Requirements
+Text editor i.e Visual Studio Code version control i.e Git Github To access this application, you can clone to your local machine using the following command; $ git clone https://github.com/patrickrop-cloud/Gallery . Cd into the app folder, then open using your favourite text editor.
+
+#Known Bugs
+As at now, the app is not fully customized yet. Pull requests for improvement of the application are welcomed.
+
+#Technologies Used
+Python3
+Django and bootstrap4 postgres db HTML
+
+#Dependencies
+
+
+#Support and contact details
+In case of any issues,and questions, contact me through; Github: https://github.com/patrickrop-cloud Twitter: @patohrop09
+
+License
+This project is under;
+
+MIT license
+MIT Copyright (c) 2021
+
--- photos/tests.py
+++ photos/tests.py
@@ -5,70 +5,22 @@ from photos.models import Image,Location,Category
 class ImageTestClass(TestCase):
     #setup method
     def setUp(self):
-        self.image1=Image(image="image",name='Flower',description="Nice image")
+        self.image1=Image(image="image",name='Car',description="Nice Car!")
 
     #Testing Instance
     def test_instance(self):
         self.assertTrue(isinstance(self.image1,Image))
 
+    #Test to save images
     def test_save_images(self):
         self.image1.save_image()
         images=Image.objects.all()
         self.assertTrue(len(images)>0)
+
+    #Test to delete saved images
     def test_delete_images(self):
         self.image1.save_image()
         images_record=Image.objects.all()
         self.image1.delete_image()
         self.assertTrue(len(images_record)==0)
-
-    def test_update_image(self):
-        image=Image.objects.first()
-        new_image=Image.update_image()
-        expected_image=f'{new_image}'
-        self.assertTrue(expected_image,'new_image')
-
-    def test_search_category(self):
-        category=Image.objects.all()
-        search_term='food'
-        db_term=search_term
-        if db_term !=search_term:
-            return('no match')
-
-        else:
-            return(search_term)  
-class CategoryTestClass(TestCase):
-    #setup method
-    def setUp(self):
-        self.nature=Category(category_name="nature")
-    #Testing Instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.nature,Category))
-
-    def test_save_categories(self):
-        self.nature.save_categories()
-        category=Category.objects.all()
-        self.assertTrue(len(category)>0)
-    def test_delete_categories(self):
-        self.nature.save_categories()
-        category_record=Category.objects.all()
-        self.nature.delete_category()
-        self.assertTrue(len(category_record)==0)
-
-class LocationTestClass(TestCase):
-    #setup method
-    def setUp(self):
-        self.nairobi=Location(location_name="Eldoret")
-    #Testing Instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.nairobi,Location))
-
-    def test_save_location(self):
-        self.nairobi.save_location()
-        location=Location.objects.all()
-        self.assertTrue(len(location)>0)
-
-    def test_delete_location(self):
-        self.nairobi.save_location()
-        location_record=Location.objects.all()
-        self.nairobi.delete_location()
-        self.assertTrue(len(location_record)==0)
+    
